7ec6ea996ea5cb272d8e867c9f9c407eaa755ce3
Harold <sadcah@yahoo.com>
2023-08-29 12:01:48 +0100

    feat:applications page rendering

--- client/app/applications/page.tsx
+++ client/app/applications/page.tsx
@@ -1,10 +1,27 @@
 'use client';
 
+import { RootState } from '@/redux/store';
+import { useSelector } from 'react-redux';
+import { TypeApplication } from '../../../server/types/types';
+
 export default function Applications() {
+  const currentUser = useSelector(
+    (state: RootState) => state.currentUserReducer.value
+  );
+  console.log(currentUser.applications);
+  console.log(currentUser);
   return (
     <main>
       <div>
         <h1 className='header'>Applications Page</h1>
+        <div className='applications-container'>
+          {currentUser.applications.map((app: TypeApplication) => (
+            <div className='applications' key={app._id as unknown as string}>
+              <p>{app.company}</p>
+              <p>{app.status}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -7,11 +7,13 @@ import Interviews from '../interviews/page';
 export default function Dashboard() {
   return (
     <main>
-      <div>
+      <div className='main-body'>
         <h1 className='header'>Dashboard</h1>
-        <Interviews />
-        <Applications />
-        <Archives />
+        <div>
+          <Interviews />
+          <Applications />
+          <Archives />
+        </div>
       </div>
     </main>
   );
