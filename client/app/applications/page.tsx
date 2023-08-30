158d1153b7e394805d10be6ad5e9ec8c2f6ee8d2
Harold <sadcah@yahoo.com>
2023-08-30 11:20:47 +0100

    feat:modal content added

--- client/app/applications/page.tsx
+++ client/app/applications/page.tsx
@@ -1,18 +1,39 @@
 'use client';
 
+import React, { useRef, useEffect, useState } from 'react';
 import { RootState } from '@/redux/store';
 import { useSelector } from 'react-redux';
 import { TypeApplication } from '../../../server/types/types';
 import ReactModal from 'react-modal';
-import { useState } from 'react';
 import '../../(components)/(css)/dashboard.css';
 
+const initialValue = {
+  company: '',
+  date: '',
+  nextInterview: '',
+  status: '',
+};
 export default function Applications() {
   const currentUser = useSelector(
     (state: RootState) => state.currentUserReducer.value
   );
+  const [modalContent, setModalContent] = useState(initialValue);
   const [showModal, setShowModal] = useState(false);
+  const appElement = useRef<HTMLElement | null>(null);
+  useEffect(() => {
+    appElement.current = document.body;
+    ReactModal.setAppElement(appElement.current);
+  }, []);
   console.log(currentUser.applications);
+  const handleModalValue = (
+    company: string,
+    date: string,
+    nextInterview: string,
+    status: string
+  ) => {
+    setModalContent({ company, date, nextInterview, status });
+    console.log(modalContent);
+  };
   return (
     <main className='container'>
       <div className='header-container'>
@@ -26,6 +47,12 @@ export default function Applications() {
             className='application'
             onClick={() => {
               setShowModal(true);
+              handleModalValue(
+                app.company,
+                app.date as unknown as string,
+                app.nextInterview?.date as string,
+                app.status
+              );
             }}
             key={app._id as unknown as string}
           >
@@ -35,14 +62,23 @@ export default function Applications() {
         ))}
       </div>
       <button className='btn-1'>View Applications</button>
-      <ReactModal isOpen={showModal} contentLabel='Minimal Modal Example'>
-        <button
-          onClick={() => {
-            setShowModal(false);
-          }}
-        >
-          <button className='btn-plus'>X</button>
-        </button>
+      <ReactModal isOpen={showModal}>
+        <div className='modal-contents'>
+          <button
+            onClick={() => {
+              setShowModal(false);
+            }}
+            className='btn-plus'
+          >
+            X
+          </button>
+          <div className='modal-content'>
+            <p>Company: {modalContent.company}</p>
+            <p>Date Of Application: {modalContent.date}</p>
+            <p>Next Interview{modalContent.nextInterview}</p>
+            <p>Application Status: {modalContent.status}</p>
+          </div>
+        </div>
       </ReactModal>
     </main>
   );
