7ed00e0b7fa13759181106f93af0b22bfeb9214a
Harold <sadcah@yahoo.com>
2023-10-04 19:42:50 +0100

    feat:added to apply reminder on profile

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -2,18 +2,20 @@
 
 import { useEffect, useState } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
-import { Doughnut } from 'react-chartjs-2';
-import { Chart, ArcElement } from 'chart.js';
+import { Button } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 import { loginUser } from '@/redux/features/currentUserSlice';
 import { RootState } from '@/redux/store';
 import { getUser } from '@/utils/APIservices';
 import { useDispatch, useSelector } from 'react-redux';
 import { TypeApplicationResponse } from '@/utils/types';
 import '../../(components)/(css)/profile.css';
-import { getComputedStyleValue } from '@/utils/utils';
-Chart.register(ArcElement);
+import InterviewApplicationItem from '@/(components)/(tsx)/InterviewApplicationItem';
+import { useRouter } from 'next/navigation';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const router = useRouter();
   const currentUser = useSelector(
     (state: RootState) => state.currentUserReducer.value
   );
@@ -21,8 +23,8 @@ export default function Profile() {
   const [interviewing, setInterviewing] = useState<TypeApplicationResponse[]>(
     []
   );
+  const [toApply, setToApply] = useState<TypeApplicationResponse[]>([]);
   const [noResponse, setNoResponse] = useState<TypeApplicationResponse[]>([]);
-  const dispatch = useDispatch();
   useEffect(() => {
     if (!currentUser.username) {
       getUser().then((res) => {
@@ -32,6 +34,8 @@ export default function Profile() {
             setRejected((prevVal) => [...prevVal, app]);
           } else if (app.nextInterview) {
             setInterviewing((prevVal) => [...prevVal, app]);
+          } else if (app.status == 'To Apply') {
+            setToApply((prevVal) => [...prevVal, app]);
           } else {
             setNoResponse((prevVal) => [...prevVal, app]);
           }
@@ -40,41 +44,8 @@ export default function Profile() {
     }
   }, [currentUser]);
 
-  const data = {
-    labels: ['Interviewing', 'Rejected', 'No Response'],
-    datasets: [
-      {
-        data: [interviewing.length, rejected.length, noResponse.length],
-        backgroundColor: [
-          getComputedStyleValue('--success-colour'),
-          getComputedStyleValue('--warning-colour'),
-          getComputedStyleValue('--error-colour'),
-        ],
-      },
-    ],
-  };
-
-  const options = {
-    plugins: {
-      legend: {
-        display: true,
-        position: 'bottom',
-        labels: {
-          color: 'white',
-          font: {
-            size: 18,
-          },
-          generateLabels: function (chart: any) {
-            return chart.data.labels.map((label: any, i: any) => ({
-              text: label,
-              fillStyle: chart.data.datasets[0].backgroundColor[i],
-              hidden: false,
-              index: i,
-            }));
-          },
-        },
-      },
-    },
+  const handleItemClick = () => {
+    console.log('click');
   };
 
   return (
@@ -84,35 +55,64 @@ export default function Profile() {
         <div className='stat-container'>
           <h4>Applications Statistics</h4>
           <div className='charts-container'>
-            <PieChart
-              data={[
-                {
-                  title: 'Interviewing',
-                  value: interviewing.length,
-                  color: 'var(--success-colour)',
-                },
-                {
-                  title: 'Rejected',
-                  value: rejected.length,
-                  color: 'var(--warning-colour)',
-                },
-                {
-                  title: 'No Response',
-                  value: noResponse.length,
-                  color: 'var(--error-colour)',
-                },
-              ]}
-              animate={true}
-              label={({ dataEntry }) =>
-                `${dataEntry.title}: ${dataEntry.value}`
-              }
-              labelStyle={{
-                fontSize: '5px',
-                fill: 'var(--text-colour)',
-              }}
-            />
-            <Doughnut data={data} options={options} />
-            <div></div>
+            <div className='reminders-container'>
+              <section className='to-apply'>
+                <h4>Do not forget to finish your application with:</h4>
+                {toApply.length ? (
+                  toApply.map((app: TypeApplicationResponse) => {
+                    return (
+                      <InterviewApplicationItem
+                        key={app._id as unknown as string}
+                        item={app}
+                        secondItem={null}
+                        onItemClick={handleItemClick}
+                      />
+                    );
+                  })
+                ) : (
+                  <p>You have no unfinished application</p>
+                )}
+                <p className='button-container'>
+                  <Button
+                    variant='contained'
+                    endIcon={<SendIcon />}
+                    style={{ backgroundColor: '#7ea3af', color: '#333333' }}
+                    onClick={() => router.push('/applications')}
+                  >
+                    Go To Applications
+                  </Button>
+                </p>
+              </section>
+            </div>
+            <div className='chart'>
+              <PieChart
+                data={[
+                  {
+                    title: 'Interviewing',
+                    value: interviewing.length,
+                    color: 'var(--success-colour)',
+                  },
+                  {
+                    title: 'Rejected',
+                    value: rejected.length,
+                    color: 'var(--warning-colour)',
+                  },
+                  {
+                    title: 'No Response',
+                    value: noResponse.length,
+                    color: 'var(--error-colour)',
+                  },
+                ]}
+                animate={true}
+                label={({ dataEntry }) =>
+                  `${dataEntry.title}: ${dataEntry.value}`
+                }
+                labelStyle={{
+                  fontSize: '5px',
+                  fill: 'var(--text-colour)',
+                }}
+              />
+            </div>
           </div>
         </div>
       </section>
